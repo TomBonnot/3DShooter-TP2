858876899c53cb6882b9ab4b2ba2308a1871f48e
JCarrier <justin.carrier@umontreal.ca>
2024-11-25 12:27:16 -0500

    Small change

--- Assets/Scripts/WeaponBehaviors/FlamethrowerBehavior.cs
+++ Assets/Scripts/WeaponBehaviors/FlamethrowerBehavior.cs
@@ -11,18 +11,16 @@ public class FlamethrowerBehavior : WeaponBehavior
     //Main variable visible on editor
     [Range(0, 300)][SerializeField] protected float boostForceHorizontal = 5f;
     [Range(0, 300)][SerializeField] protected float boostForceVertical = 5f;
-
-    private GameObject player;
     private Rigidbody playerRB;
-
     private GameObject flame;
+    private Flamethrower flamethrower;
 
     void Awake()
     {
         this.projectile = new Flame(_projectile, _bulletSpeed, _bulletLieftime);
-        this.weapon = new Flamethrower(_weaponPrefab, _gunPoint, projectile, _maxAmmo, boostForceVertical, boostForceHorizontal);
-        player = GameObject.FindWithTag("Player");
-        playerRB = player.GetComponent<Rigidbody>();
+        this.flamethrower = new Flamethrower(_weaponPrefab, _gunPoint, projectile, _maxAmmo, boostForceVertical, boostForceHorizontal);
+        this.weapon = this.flamethrower;
+        playerRB = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
     }
 
     public override void Shoot()
@@ -32,12 +30,11 @@ public class FlamethrowerBehavior : WeaponBehavior
 
     public override void ShootHeld()
     {
+        // Hover boost
         if (!weapon.expendsAmmo()) { destroyFlame(); return; }
         Vector3 horizVector = boostHorizontally();
         Vector3 vertVector = boostVertically();
         playerRB.AddForce(horizVector + vertVector, ForceMode.Force);
-        Debug.DrawRay(player.transform.position, horizVector, Color.red);
-        Debug.DrawRay(player.transform.position, vertVector);
     }
 
     public override void ReleaseShoot()
@@ -53,15 +50,11 @@ public class FlamethrowerBehavior : WeaponBehavior
 
     private Vector3 boostVertically()
     {
-        Vector3 forceVector =
-            new Vector3(0, ((Flamethrower)weapon).getVerticalBoostStrength(playerRB.linearVelocity) * -1, 0);
-        return forceVector;
+        return new Vector3(0, flamethrower.getVerticalBoostStrength(playerRB.linearVelocity) * -1, 0);
     }
 
     private Vector3 boostHorizontally()
     {
-        Vector3 forceVector =
-            new Vector3(weapon.gunPoint.transform.up.x, 0, weapon.gunPoint.transform.up.z) * -1 * ((Flamethrower)weapon).getHotizontalBoostStrength();
-        return forceVector;
+        return new Vector3(weapon.gunPoint.transform.up.x, 0, weapon.gunPoint.transform.up.z) * -1 * flamethrower.getHotizontalBoostStrength();
     }
 }
